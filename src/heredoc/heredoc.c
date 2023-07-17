d2b2731d54c15160353e06398618260815323a45
Pierre-Alexandre Delannoy <pdelanno@c4c2c2.42berlin.de>
2023-07-17 12:19:04 +0200

    heredoc

---
+++ src/heredoc/heredoc.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pdelanno <pdelanno@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 11:32:47 by pdelanno          #+#    #+#             */
+/*   Updated: 2023/07/17 12:18:48 by pdelanno         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	create_heredoc(char *file_name)
+{
+	int fd;
+	char *line;
+
+	fd = open(file_name, O_CREAT);
+	line = readline(HEREDOC_MSG);
+	while(line)
+	{
+		write(fd, line, ft_strlen(line));
+		write(fd, "\n", 1);
+		free(line);
+		line = readline(HEREDOC_MSG)
+	}
+	free(line);
+	//stop heredoc
+	close(fd);
+	return (EXIT_SUCCESS);
+}
+
+char *create_heredoc_filename(void)
+{
+}
+
+int ft_heredoc()
+{
+create_heredoc
+}
+
+int	heredoc()
+{
+	int es;
+
+	es = EXIT_SUCCESS;
+	//cmd->redirections
+	//cmd-><file_name> create_heredoc_filename
+	es = ft_heredoc;
+	return (EXIT_SUCCESS);
+}
