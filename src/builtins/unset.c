59968ca9e7fcfd096f7697630615d87b068f2f8b
Pierre-Alexandre Delannoy <pdelanno@c4c2c1.42berlin.de>
2023-07-19 10:14:29 +0200

    unset

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -11,3 +11,44 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int unset_error(char **str, t_shell **tmp)
+{
+    int i;
+
+    i = 0;
+    if (str[1] == '\0')
+    {
+        printf("minishell: unset: not enough arguments");
+        return (1);
+    }
+    while (str[1][i])
+    {
+        if (str[1][i] == '/')
+        {    
+            printf(minishell: unset: %s: not a valid identifier, str[1]);
+            return (EXIT_FAILURE);
+        }
+    }
+    return (EXIT_SUCCESS);
+}
+
+int unset(t_shell *cmd)
+{
+    t_shell **tmp;
+    char       **str;
+
+    tmp = &cmd;
+    if (ft_findchar((*tmp)->tokens->command, '=' == 0))
+    {
+        printf("minishell: unset: not a valid identifier");
+        return (EXIT_FAILURE);
+    }
+    str = ft_split(((*tmp)->tok_lst->command), "=");
+    if (unset_error(str) == 1)
+        return (EXIT_FAILURE);
+    else
+        ft_lstdelone((*tmp)->env_lst, str);
+    free_array(str);
+    return (EXIT_SUCCESS);
+}
