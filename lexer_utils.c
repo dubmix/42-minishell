84b0c56fca848b9341628733533a5684a5a4f2ad
EmilieD8 <103639810+EmilieD8@users.noreply.github.com>
2023-07-19 10:14:42 +0200

    Add files via upload

--- lexer_utils.c
+++ lexer_utils.c
@@ -1,5 +1,17 @@
 #include "minishell.h"
 
+void    free_arr(char **arr)
+{
+    int i;
+    
+    i = 0;
+    while (arr[i] != NULL)
+    {
+        free(arr[i]);
+        i++;
+    }
+}
+
 
 void    print_list(t_env *env) // print the envp
 {
@@ -9,6 +21,7 @@ void    print_list(t_env *env) // print the envp
             printf("node is %s and value is %s\n", tmp->name, tmp->value);
             tmp = tmp->next;
     }
+    
 }
 
 void    print_list_tok(t_token *tok)
@@ -16,7 +29,16 @@ void    print_list_tok(t_token *tok)
     t_token *tmp = tok;
     while (tmp)
     {
-        printf("node is %s, number is %d, state is %d, type is %d\n", tmp->command, tmp->nb, tmp->state, tmp->type);
+        printf("node is %s, number is %d, state is %d, type is %d\n", tmp->command, tmp->index, tmp->state, tmp->type);
         tmp = tmp->next;
     }
 }
+
+int	special_char(int c)
+{
+	if (c == '|' || c == 34 || c == ' ' || c == '<' || c == '>' || c == 39 || c == 9)
+        return (1);
+	else
+		return (0);
+}
+
--- minishell.h
+++ minishell.h
@@ -5,10 +5,9 @@
     # include <stdio.h>
     # include <readline/readline.h>
     # include <readline/history.h>
-    // # include "libft/libft.h"
+    # include "libft/libft.h"
     # include <string.h>
     # include <unistd.h>
-
 
 enum e_type
 {
@@ -27,7 +26,7 @@ enum e_type
 typedef struct s_token
 {
     char *command;
-    int nb;
+    int index;
     enum e_type type;
     struct s_token *next;
     int state; // 0 if nowhere, 1 if in double quote, 2 if in single quote
@@ -45,8 +44,20 @@ typedef struct s_shell
     char *line_command;
     int d_quote;
     int s_quote;
-    t_token *tokens;
-    t_env   *envir;
+    t_token *tok_lst;
+    t_env   *env_lst;
+    char **envp_copy; // export function recre2er la char ** // do we really need something else than the path
+    int size_arr_var;
+    int redir_in;
+    int redir_out;
+    int heredoc;
+    int append;
+    char **redir_in_arr;
+    char **redir_out_arr;
+    char **heredoc_arr;
+    char **append_arr;
+    int pipe_number;
+    int number_token;
 } t_shell;
 
 
@@ -56,23 +67,31 @@ int minishell_start(char **envp);
 /* lexer_utils.c */
 void    print_list(t_env *env); // print the envp
 void    print_list_tok(t_token *tok); // print the token list
+void    free_arr(char **arr); // free any arrays 
+int	special_char(int c);
 
 /* lexer_token.c */
 void add_stack_back_tok(t_token **tok_lst, t_token *new);
 void new_token(t_token **tokens, char *command, int nb, enum e_type type);
-int	special_char(int c);
 int new_token_var_words(t_token **tokens, char *string, int i, int nb_token);
 int new_token_quote(t_token **tokens, char *string, int i, int nb_token);
 t_token *tokenization(t_shell *shell);
 
 /* lexer_envp.c */
+
+char **string_variables(t_shell *cmd, t_token *var);
+void double_quote_env(t_shell *cmd, t_token *var);
+void expand_var(t_shell *cmd);
+void look_into_envir(t_shell *cmd, t_token *var);
+char *look_into_envir_quote(t_shell *cmd, char *variable);
+
+/* lexer_envp2.c */
+int length_arr_var(char **arr_var, t_shell *cmd);
+int length_string_without_var(char *string);
 void add_stack_back_env(t_env **env_lst, t_env *new);
 void new_node_env(t_env **env_list, char **string);
-t_env *init_envp(char **envp);
-void look_into_envir(t_shell *cmd, t_token *var);
-void expand_var(t_shell *cmd);
-char *look_into_envir_quote(t_shell *cmd, char *variable);
-void    copy_expand_var(t_shell *cmd, t_token *var, int start, int i);
+t_env *init_envp(char **envp, t_shell *cmd);
+
 
 /* libft */
 int	countsubstr(char const *s, char c);
@@ -84,10 +103,20 @@ char	*checksub(void);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char *s1, char *s2);
 
+/*parser1.c*/
+void    parser(t_shell *cmd);
+void    triage_cmd_redir(t_shell *cmd);
+void    deleteNode(t_token **head, t_token *nodeToDelete);
+void    triage_space(t_shell *cmd);
+
+/*parser_single_command.c*/
+void   single_command(t_shell *cmd);
+
 /* TO DO 
 error handling, usually I put error in the printf statement 
-free all the struct
-
+free all the struct for each new prompt
+handle ?* in the expander
+handle history
 */
 
 #endif
