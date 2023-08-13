b6af35f26bbb707973b493b118679484b2f0bf02
Emilie Drouot <edrouot@c4a7c10.42berlin.de>
2023-08-13 15:22:46 +0200

    norminette builtins

--- minishell.h
+++ minishell.h
@@ -122,8 +122,8 @@ char	*double_quote_env_bis(char *command, char *new_string,
 int	double_quote_env_bis_sub(char *command, int i);
 
 /* lexer_utils.c */
-void    print_list(t_env *env); // print the envp
-void    print_list_tok(t_token *tok); // print the token list
+void    print_list(t_env *env);
+void    print_list_tok(t_token *tok);
 int         special_char(int c);
 int         length_arr_var(char **arr_var, t_shell *cmd);
 int         length_string_without_var(char *string);
@@ -245,12 +245,13 @@ int	ft_findchar(char *str, char c);
 int var_exists(t_env *env, char *str);
 int	ft_lstsize_env(t_env *lst);
 void	update_envp_copy(t_shell *cmd);
-void print_sorted_env(t_env **env_lst);
+int     print_sorted_env(t_env **env_lst);
 void	assign_index(t_env **env_lst, char *string, int i);
 char *find_biggest(t_env **env_lst);
 void	sort_env(t_env **env_lst);
 void	export_variable_bis(t_shell *cmd, char **command, int i, t_shell **tmp);
 void	assign_back_to_zero(t_env **env_lst);
+void	export_bis(char **command, t_shell **tmp, int i, t_shell *cmd);
 
 /*unset*/
 int unset(t_shell *cmd, char **command);
