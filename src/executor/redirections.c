a2abc2a543d18e806621d98e5bedca9009cda1b7
Pierre-Alexandre Delannoy <pdelanno@c4c2c1.42berlin.de>
2023-08-14 11:09:58 +0200

    norminette

--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -90,7 +90,7 @@ int	exec_infile(char *file)
 		ft_putstr_fd("minishell: ", STDERR_FILENO);
 		ft_putstr_fd(file, STDERR_FILENO);
 		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
-		return (EXIT_FAILURE);
+		exit (EXIT_FAILURE);
 	}
 	dup = dup2(fd, STDIN_FILENO);
 	if (fd > 0 && dup < 0)
